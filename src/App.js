882a86ef7a8802879884a9003ee0bbaed4b46cbc
Gopi2923 <dgopidhikshan@gmail.com>
2025-02-16 22:23:21 +0530

    Update routing to use react-router-dom v6; add About, Contact, and Error components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Header from './components/Header';
 import Body from './components/Body';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Error from './components/Error';
+import Contact from './components/Contact';
+import About from './components/About';
+
 
 const styleCard = {
   
@@ -16,9 +21,25 @@ const AppLayout = () => {
   )
 }
 
+const appRouter = createBrowserRouter([
+  {
+    path: '/',
+    element: <AppLayout />,
+    errorElement: <Error />
+  },
+  {
+    path: '/about',
+    element: <About />,
+  },
+  {
+    path: '/contact',
+    element: <Contact />
+  }
+]);
+
 const root = ReactDOM.createRoot(document.getElementById('root')); // It's the job of ReactDOM to create root i.e, heading in this case, bcz root is a dom element which is rendered to be on browser. This root is the place where all React code will run.
 
 // root.render(heading);
-root.render(<AppLayout />);
+root.render(<RouterProvider router={appRouter} />);
 
 // In React, Render is the technique that can redirect a page with the help of function render(). Most importantly, render a function we can use to define the HTML code within the HTML element. It helps to display certain views in the UI using certain logic defined in the render function and returns the output.
---
+++ src/components/Contact.js
@@ -0,0 +1,11 @@
+import react from 'react';
+
+const Contact = () =>  {
+    return (
+        <div>
+            Contact Us
+        </div>
+    )
+}
+
+export default Contact;
