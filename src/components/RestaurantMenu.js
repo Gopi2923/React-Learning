b9ee7b74858d967d50b7de57d6e08f1b4eead71d
Gopi2923 <dgopidhikshan@gmail.com>
2025-02-18 23:50:24 +0530

    Fetch restaurant data from Swiggy API and display restaurant name in RestaurantMenu component

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -6,18 +6,22 @@ const ResaurantMenu = () => {
 
     useEffect(() => {
         fetchData();
-    })
+    }, []);
 
     const fetchData = async () => {
-        const data = await fetch('');
+        const data = await fetch('https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9414991&lng=77.62358259999999&restaurantId=229&catalog_qa=undefined&submitAction=ENTER');
         const json = await data.json();
 
         console.log(json);
+        // setResInfo(json?.data?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card?.itemCards[0]?.card?.info)
         setResInfo(json.data);
     }
+
+    const {name, cusines} = resInfo?.cards[0]?.card?.card?.text;
+
     return (
-        <div className="menu">
-            <h1>Name of Restaurant</h1>
+        <div className="menu"> \
+            <h1> {name} </h1>
             <h2>Menu</h2>
         </div>
     )
